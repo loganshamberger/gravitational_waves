0121bb770ea16aaa2a8711140906388cc2d6fc47
Logan Shamberger <loganshamberger@gmail.com>
2026-07-28 21:00:39 -0400

    moved schwarzchild geodesic to geodesics folder in anticipation of comparing with kerr and RN blackhole

--- 1_particle_orbiting_kerr_black_hole/simulator.py
+++ 1_particle_orbiting_kerr_black_hole/simulator.py
@@ -10,6 +10,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "shared"))
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)),".","geodesics"))
 from core import run_from_config
 from schwarzschild import SchwarzschildGeodesic, SchwarzschildPhotonGeodesic
 
