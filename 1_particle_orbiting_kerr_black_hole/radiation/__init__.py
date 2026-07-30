1df3740da104657eb819fa4db6183071ef3b0e81
Logan Shamberger <loganshamberger@gmail.com>
2026-07-30 18:01:52 -0400

    Needed dose of sarcasm

--- 1_particle_orbiting_kerr_black_hole/radiation/__init__.py
+++ 1_particle_orbiting_kerr_black_hole/radiation/__init__.py
@@ -1,2 +1,5 @@
 ## I'm now questioning the validity of this model. 
 # MTW Gravitation and Clifford Well's book on Gravity might have some better answers
+#
+# Moral of the story: a day in the library saves six months in the lab
+# (or 6 million in tokens...)
